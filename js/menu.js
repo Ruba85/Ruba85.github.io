28d1acaa4a3a8deca016df59e7a95a67f76a60cf
Serhii Rubalchenko <sruba85@gmail.com>
2024-06-27 09:43:26 +0300

    test css and js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
-let clicks = [].slice.call(document.getElementsByClassName("main_list"));
-let menu = [].slice.call(document.getElementsByClassName("main_menu"));
-for (var i = 0, len = clicks.length; i < len; i++);
-{
-  (function (index) {
-    clicks[i].onclick = function () {
-      menu.map((m, i) => {
-        if (i === index) {
-          m.classList.toggle("show");
-        } else {
-          m.classList.remove("show");
-        }
-      });
-    };
-  })(i);
-}
+window.onload = function () {
+  let clicks = [].slice.call(document.getElementsByClassName("main_list"));
+  let menu = [].slice.call(document.getElementsByClassName("main_menu"));
+  for (var i = 0, len = clicks.length; i < len; i++);
+  {
+    (function (index) {
+      clicks[i].onclick = function () {
+        menu.map((m, i) => {
+          if (i === index) {
+            m.classList.toggle("show");
+          } else {
+            m.classList.remove("show");
+          }
+        });
+      };
+    })(i);
+  }
+};
