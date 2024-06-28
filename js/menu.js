0a80feecf845323da8e527b0659f09789f31d8fe
Serhii Rubalchenko <sruba85@gmail.com>
2024-06-28 10:48:43 +0300

    add to github

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,12 @@
 window.onload = function () {
-  let clicks = [].slice.call(
-    document.getElementsByClassName("menu__main-list")
+  let clicks = [].slice.call(document.getElementsByClassName("crm__menu-link"));
+  let menuList = [].slice.call(
+    document.getElementsByClassName("crm__menu-list")
   );
-  let menu = [].slice.call(document.getElementsByClassName("menu__main"));
-  console.log(clicks, menu);
   for (var i = 0, len = clicks.length; i < len; i++)
     (function (index) {
       clicks[i].onclick = function () {
-        menu.map((m, i) => {
+        menuList.map((m, i) => {
           if (i === index) {
             m.classList.toggle("show");
           } else {
@@ -16,4 +15,15 @@ window.onload = function () {
         });
       };
     })(i);
+  let menu = document.getElementById("crm__menu");
+  let status = document.getElementById("menu-burger");
+  let burger = [].slice.call(document.getElementsByClassName("toggle-menu"));
+  burger.map(function (e) {
+    e.addEventListener("click", toggleMenu);
+  });
+  function toggleMenu() {
+    menu.classList.toggle("show");
+    status.classList.toggle("open");
+    document.body.classList.toggle("fixer");
+  }
 };
